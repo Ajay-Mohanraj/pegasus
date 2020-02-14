a5eeec06690170e33071746d03554d611ec2efdc
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2020-02-13 19:39:38 -0500

    board pane added

--- src/Board.java
+++ src/Board.java
@@ -2,7 +2,8 @@ import javafx.application.Application;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-public class Board extends Pane {
+public class Board extends GridPane {
+	public GridPane boardPane = new GridPane();
 	public Board() {
 
 	}
