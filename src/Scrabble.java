d3af4800d07b0105eff15beca5b11cbcd6c35593
davkri22 <davkri22@bergen.org>
2020-02-13 23:16:22 -0500

    finished board layout

--- src/Scrabble.java
+++ src/Scrabble.java
@@ -9,9 +9,11 @@ public class Scrabble extends Application {
     private final char[] alphabet = "abcdefghijklmnopqrstuvwxyz ".toCharArray();
     private int[] amounts = new int[] {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1, 2};
     private ArrayList<Tile> tiles = new ArrayList<>();
+    private ArrayList<Character> bag = new ArrayList<>();
     private int tilesGiven = 0;
 
     public void start(Stage stage) {
+        updateBag();
         giveTiles();
         Board board = new Board();
 
@@ -26,8 +28,12 @@ public class Scrabble extends Application {
         int needed = 7 - tiles.size();
         int bagSize = 100 - tilesGiven;
         for (int i = 0; i < bagSize; i++){
-
+            int tileIndex = 0;
         }
+    }
+
+    public void updateBag(){
+
     }
 }
 
