6040d707020240e43c11f97ac4b43907dbd139bf
davkri22 <davkri22@bergen.org>
2020-02-13 20:45:57 -0500

    made double word diagonals

--- src/Board.java
+++ src/Board.java
@@ -12,6 +12,15 @@ public class Board extends GridPane {
 	    setHeight(600);
 		for (int row = 0; row < 15; row++) {
 			for (int column = 0; column < 15; column++) {
+				Normal m = new Normal();
+				m.setFill(Color.SANDYBROWN);
+				m.setHeight(getHeight() / 15);
+				m.setWidth(getWidth() / 15);
+				add(m, column, row);
+			}
+		}
+		for (int row = 0; row < 15; row++) {
+			for (int column = 0; column < 15; column++) {
 			    if (row == column || row + column == 14) {
                     MultWord m = new MultWord(2);
                     m.setFill(Color.RED);
@@ -19,6 +28,27 @@ public class Board extends GridPane {
                     m.setWidth(getWidth() / 15);
                     add(m, column, row);
                 }
+				else if ((row == 0 || row == 14) && (column == 0 || column == 14)){
+					MultWord m = new MultWord(2);
+					m.setFill(Color.LIGHTBLUE);
+					m.setHeight(getHeight() / 15);
+					m.setWidth(getWidth() / 15);
+					add(m, column, row);
+				}
+			    else if ((row == 0 || row == 14) && (column == 3 || column == 11)){
+					MultLetter m = new MultLetter(2);
+					m.setFill(Color.LIGHTBLUE);
+					m.setHeight(getHeight() / 15);
+					m.setWidth(getWidth() / 15);
+					add(m, column, row);
+				}
+				else if ((column == 0 || column == 14) && (row == 3 || row == 11)){
+					MultLetter m = new MultLetter(2);
+					m.setFill(Color.LIGHTBLUE);
+					m.setHeight(getHeight() / 15);
+					m.setWidth(getWidth() / 15);
+					add(m, column, row);
+				}
 			}
 		}
 
