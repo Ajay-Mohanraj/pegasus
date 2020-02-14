63a470575a9726339c8f0a3869228cd792046f78
davkri22 <davkri22@bergen.org>
2020-02-13 20:26:55 -0500

    Merge branch 'goodBranch' of C:\Users\divad\IdeaProjects\pegasus with conflicts.

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,12 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import tiles.Tile;
 
 public class Board extends GridPane {
 	//public GridPane boardPane = new GridPane();
 	public Board() {
-		setBackground(new Background(new BackgroundFill(Color.SANDYBROWN, CornerRadii.EMPTY, Insets.EMPTY)));
 		for (int row = 0; row < 15; row++) {
 			for (int column = 0; column < 15; column++) {
 				if (row == column) {
